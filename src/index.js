cc6e9c2838060f0c7988ab5b2756690bbcce99e6
xiaxinyao1 <xiaxinyao1@jd.com>
2022-06-27 17:31:16 +0800

    feat: 完成HTML、CSS、JavaScript的基本配置

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './index.scss'
+
+const img = new Image()
+img.src = './logo.svg'
+
+document.querySelector('#root').appendChild(img)
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const babelOptions = require('./babel.config')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js', // 打包入口
+  output: {
+    filename: '[name].bundle.js', // 输出文件名
+    path: path.join(__dirname, 'dist') // 输出文件目录
+  },
+  module: { 
+    rules: [ // 转换规则
+    {
+        test: /\.(js|ts)x?$/i,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: babelOptions
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader,'css-loader','postcss-loader'] // 对应的 Loader 名称
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: [MiniCssExtractPlugin.loader,'css-loader','postcss-loader','sass-loader'] // 对应的 Loader 名称
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          // 输出文件位置以及文件名
+          filename: "images/[hash][ext]"
+        },
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
+        type: 'asset/resource',
+        generator: {
+          // 输出文件位置以及文件名
+          filename: "fonts/[hash][ext]"
+        },
+      },
+      {
+        test: /\.svg$/,
+        type: 'asset/source',
+      },
+    ]
+  },
+  plugins:[ // 配置插件
+    new HtmlWebpackPlugin({
+        template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+        filename: '[name].[hash:8].css'
+      }),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, 'public'), // 静态文件目录
+    compress: true, //是否启动压缩 gzip
+    port: 8080, // 端口号
+    // open:true  // 是否自动打开浏览器
+  }
+}
