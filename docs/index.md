2fe277c85c0ec155e5d799307413258036085e23
xiaxinyao1 <xiaxinyao1@jd.com>
2022-06-28 13:37:24 +0800

    docs: 新增todo文档

---
+++ docs/index.md
@@ -0,0 +1,19 @@
+# TODOS
+
+#### feature
+
+- [x] 支持 图片文件（jpg、png、svg）的引入
+
+- [ ] 代码格式化&合规检测（ESLint + Prettier）
+
+- [ ] 支持 CSS Modules 的使用
+
+- [ ] index.html 配置化
+
+#### fix
+
+- [ ] 修复引入 index.tsx 时的报错（别名配置）
+
+- [ ] 修复图片格式 ts 类型报错（tsconfig.json研究）
+
+- [ ] 修复配置目录变化后 postcss 不生效问题
--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import logo from '../../assets/images/logo.svg'
 import './index.scss'
 
 const App = () => {
   return (
     <div className='app'>
+      <span
+        dangerouslySetInnerHTML={{
+          __html: logo,
+        }}
+      />
       Welcome to React CLI!
     </div>
   );
