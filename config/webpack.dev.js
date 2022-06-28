332d354721ae9a99a68cd07023f1f473e6f98ea8
xiaxinyao1 <xiaxinyao1@jd.com>
2022-06-28 17:57:04 +0800

    style: prettier命令配置

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,12 +3,12 @@ const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'eval-cheap-module-source-map',
-    devServer: {
-        static: path.resolve(__dirname, 'public'), // 静态文件目录
-        compress: true, //是否启动压缩 gzip
-        port: 8080, // 端口号
-        // open:true  // 是否自动打开浏览器
-      }
+  mode: 'development',
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    static: path.resolve(__dirname, 'public'), // 静态文件目录
+    compress: true, //是否启动压缩 gzip
+    port: 8080, // 端口号
+    // open:true  // 是否自动打开浏览器
+  },
 })
