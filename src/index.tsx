8a079e4a4c777539c5ca4202a8aa1a1999b5c2aa
xiaxinyao1 <xiaxinyao1@jd.com>
2022-06-29 14:40:03 +0800

    fix: 修复ts报错

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,4 @@ import { createRoot } from 'react-dom/client'
 
 import App from '@/pages/App'
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root') as HTMLElement).render(<App />)
