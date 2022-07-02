cd12ec9c2c67c6a20cf0a040f4cde70f0496e4b0
Lorienn <bonitaxxy@163.com>
2022-07-02 09:24:46 +0800

    fix: 修复postcss无效；scss类型补充

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -5,3 +5,8 @@ declare module '*.jpeg'
 declare module '*.gif'
 declare module '*.bmp'
 declare module '*.tiff'
+
+declare module '*.module.scss' {
+  const content: { [className: string]: string }
+  export = content
+}
